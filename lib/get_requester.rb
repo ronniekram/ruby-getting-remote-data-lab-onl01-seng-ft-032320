3c39f16f9c200e013ede4f64e67011fcddc0ee61
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-07 19:24:15 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,8 +3,11 @@ require 'open-uri'
 require 'json'
 
 class GetRequester
+  attr_accessor :URL
   
-  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+  def initialize(URL)
+    @URL = URL
+  end 
   
   def get_response_body
     uri = URI.parse(URL)
