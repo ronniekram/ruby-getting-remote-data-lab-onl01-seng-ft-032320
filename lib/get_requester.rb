b9f033266bbef3b840deb3b9a40d61424c5c1160
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-07 19:22:48 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -13,10 +13,11 @@ class GetRequester
   end 
   
   def parse_json
-  programs = JSON.parse(self.get_programs)
+    programs = JSON.parse(self.get_programs)
   
-  programs.collect do |program|
-    program["agency"]  
-  end
+    programs.collect do |program|
+      program["agency"]  
+    end
   end 
+  
 end 
